72cd7b2939b0d700703ecb9a114d44691ef1ded9
lalitarana123456 <lalitakumari39840@gmail.com>
2026-03-18 19:44:24 +0530

    updated code

--- media website/backend/models/User.js
+++ media website/backend/models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const crypto = require('crypto');
 
+
 const userSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
